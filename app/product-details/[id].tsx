150c076af329d711aa7827dc835cc1f6e4d7e892
omarbougarne <omarbougarne98@gmail.com>
2025-02-12 16:24:26 +0100

    Adding dynamic routes

--- app/product-details/[id].tsx
+++ app/product-details/[id].tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+
+
+interface Product {
+id: number;
+name: string;
+type: string;
+barcode: string;
+price: number;
+solde: number;
+supplier: string;
+image: string;
+stocks: any[];
+editedBy: any[];
+}
+
 
 export default function ProductDetails() {
+
+  
+
   return (
-    <div>
-      
-    </div>
+    <View>
+
+    </View>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  price: {
+    fontSize: 18,
+    color: 'green',
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 16,
+  },
+});
